78365c4afa1a549c342752f037b926832892e67d
priyansh agrahari <75441026+r3dacted42@users.noreply.github.com>
2026-01-12 18:37:16 +0530

    add astro site and deploy action

---
+++ src/data/awards.ts
@@ -0,0 +1,14 @@
+export default [
+  {
+    title: "Chief of Air Staff Commendation Card",
+    location: "Bagdogra, WB",
+    year: "2008",
+    description: "Awarded the Chief of the Air Staff (CAS) Commendation for meritorious service and professional excellence as Garrison Engineer, Air Force Station Bagdogra."
+  },
+  {
+    title: "Chief of Naval Staff Commendation Card",
+    location: "Mumbai, Maharashtra",
+    year: "2004",
+    description: "Awarded the Chief of the Naval Staff (CNS) Commendation for exceptional dedication and professional contribution as Garrision Engineer, INS Himla."
+  },
+];
